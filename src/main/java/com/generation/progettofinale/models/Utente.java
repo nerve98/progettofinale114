c3d98d8e982d0ae276508feb2075b3a00aeff796
GiacomoMazza33 <giacomp.mazza@gmail.com>
2024-05-16 12:43:26 +0200

    upadateUtente

--- src/main/java/com/generation/progettofinale/models/Utente.java
+++ src/main/java/com/generation/progettofinale/models/Utente.java
@@ -1,5 +1,20 @@
 package com.generation.progettofinale.models;
 
-public class Utente {
+import java.sql.Date;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class Utente extends Entity{
+    private String username, password, nome, cognome, email,numeroCellulare;
+    private Date dataNascita;
+    private boolean isAdmin;
+    
 }
+
+
+
+
+
+
