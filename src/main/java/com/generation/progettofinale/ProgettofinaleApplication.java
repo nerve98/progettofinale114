b95d33e8185ca72e081fcb9e2f320128efa50837
Gianluca <gianluca3f@gmail.com>
2024-05-16 12:25:44 +0200

    first commit

---
+++ src/main/java/com/generation/progettofinale/ProgettofinaleApplication.java
@@ -0,0 +1,13 @@
+package com.generation.progettofinale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettofinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettofinaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/progettofinale/ProgettofinaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.progettofinale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettofinaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
