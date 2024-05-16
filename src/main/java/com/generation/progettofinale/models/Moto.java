72dcaf5c7a27bdf9103d76ca45f0580059c494bb
Alessandro <alearry@gmail.com>
2024-05-16 14:12:52 +0200

    Update Moto.java

--- src/main/java/com/generation/progettofinale/models/Moto.java
+++ src/main/java/com/generation/progettofinale/models/Moto.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode(callSuper = false)
 public class Moto extends Entity{
     private int cilindrata,capacitaSerbatoio,potenzaCV,potenzaKV;
     private double peso,altezzaSella,consumo,prezzo;
