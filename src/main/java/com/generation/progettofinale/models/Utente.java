9e28761d7c17d93266d2618401f21c3a19de1354
MicheleBonafede <g.michelebonafede@gmail.com>
2024-05-16 12:44:37 +0200

    a

--- src/main/java/com/generation/progettofinale/models/Utente.java
+++ src/main/java/com/generation/progettofinale/models/Utente.java
@@ -1,11 +1,12 @@
 package com.generation.progettofinale.models;
 
 import java.sql.Date;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-@EqualsAndHashCode(callSuper = true)
+
 @Data
+@EqualsAndHashCode(callSuper = true)
+
 public class Utente extends Entity{
     private String username, password, nome, cognome, email,numeroCellulare;
     private Date dataNascita;
