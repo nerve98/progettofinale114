ad15c6f3e6a8d16ee07fecf85b4d4e249e93f8d9
Gianluca <gianluca3f@gmail.com>
2024-05-17 15:40:05 +0200

    fix classi Services

--- src/main/java/com/generation/progettofinale/Services/ServiceUtente.java
+++ src/main/java/com/generation/progettofinale/Services/ServiceUtente.java
@@ -1,8 +1,14 @@
 package com.generation.progettofinale.Services;
 
+import org.springframework.stereotype.Service;
+
 import com.generation.progettofinale.dao.DaoUtente;
 import com.generation.progettofinale.models.Utente;
 
+import lombok.Data;
+
+@Service
+@Data
 public class ServiceUtente extends GenericService<Long, Utente, DaoUtente>{
 
     public Utente findByUsernameAndPassword(String username, String password){
