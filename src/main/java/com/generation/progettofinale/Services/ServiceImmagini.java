1b8a7649464057781b697e629f2423abed87aa71
Gianluca <gianluca3f@gmail.com>
2024-05-17 09:51:45 +0200

    aggiunta mancanti Service files

---
+++ src/main/java/com/generation/progettofinale/Services/ServiceImmagini.java
@@ -0,0 +1,8 @@
+package com.generation.progettofinale.Services;
+
+import com.generation.progettofinale.dao.DaoImmagini;
+import com.generation.progettofinale.models.Immagini;
+
+public class ServiceImmagini extends GenericService<Long, Immagini, DaoImmagini>{
+    
+}
