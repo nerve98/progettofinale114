f7038b023c6976e6337d45b28d9284f54bf44682
Alessandro <alearry@gmail.com>
2024-05-21 17:40:21 +0200

    Update CascoController.java

--- src/main/java/com/generation/progettofinale/controllers/CascoController.java
+++ src/main/java/com/generation/progettofinale/controllers/CascoController.java
@@ -1,18 +1,18 @@
 package com.generation.progettofinale.controllers;
 
 import java.util.List;
-import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestParam;
 import com.generation.progettofinale.Services.ServiceCasco;
 import com.generation.progettofinale.Services.ServiceImmagini;
 import com.generation.progettofinale.models.Casco;
 import com.generation.progettofinale.models.Immagini;
-import com.generation.progettofinale.models.Utente;
+
 
 import jakarta.servlet.http.HttpSession;
 
