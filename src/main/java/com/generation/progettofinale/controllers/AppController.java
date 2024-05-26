db9671b243848c32a94f02c8db36e1b78307fbf8
Gianluca <gianluca3f@gmail.com>
2024-05-26 15:55:37 +0200

    torna alla home (da carrello) e svuota carrello

--- src/main/java/com/generation/progettofinale/controllers/AppController.java
+++ src/main/java/com/generation/progettofinale/controllers/AppController.java
@@ -20,88 +20,92 @@ import jakarta.servlet.http.HttpSession;
 // Remove the conflicting import statement
 // import org.springframework.ui.Model;
 
-
 @Controller
 @SessionAttributes("myList")
 public class AppController {
 
-@Autowired
-private ServiceUtente serviceUtente;
+    @Autowired
+    private ServiceUtente serviceUtente;
 
-@GetMapping("/acquisti")
-public String acquisti() {
-    return "paginaAcquisto.html";
-}
-@GetMapping("/pagamento")
-public String admin() {
-    return "cartaDiCredito.html";
-}
-
-@GetMapping("/home")
-public String home(Model model,
-HttpSession session) {
-    model.addAttribute("isAdmin", session.getAttribute("admin"));
-    model.addAttribute("loggato", session.getAttribute("loggato"));
-    List<Entity> carrello=(List<Entity>) session.getAttribute("carrello");
-    if(carrello!=null && carrello.size()>0){
-        model.addAttribute("numCarrello", carrello.size());
+    @GetMapping("/acquisti")
+    public String acquisti() {
+        return "paginaAcquisto.html";
     }
-    return "index.html";
-}
 
+    @GetMapping("/pagamento")
+    public String admin() {
+        return "cartaDiCredito.html";
+    }
 
-@GetMapping("/prova")
-public String prova(){
-    return "prove.html";
-}
+    @GetMapping("/home")
+    public String home(Model model,
+            HttpSession session) {
+        model.addAttribute("isAdmin", session.getAttribute("admin"));
+        model.addAttribute("loggato", session.getAttribute("loggato"));
+        List<Entity> carrello = (List<Entity>) session.getAttribute("carrello");
+        if (carrello != null && carrello.size() > 0) {
+            model.addAttribute("numCarrello", carrello.size());
+        }
+        return "index.html";
+    }
 
+    @GetMapping("/prova")
+    public String prova() {
+        return "prove.html";
+    }
 
+    @GetMapping("/login")
+    public String login() {
+        return "formLogin.html";
+    }
 
-@GetMapping("/login")
-public String login() {
-    return "formLogin.html";
-}
-
-@GetMapping("/logout")
-    public String logout(HttpSession session){
-        session.setAttribute("loggato", null);;
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.setAttribute("loggato", null);
+        ;
         session.setAttribute("utente", null);
         session.setAttribute("admin", null);
         return "redirect:/home";
     }
 
+    @PostMapping("/login")
+    public String login(@RequestParam("username") String username,
+            @RequestParam("password") String password, Model model,
+            HttpSession session) {
+        Utente utenteLoggato = serviceUtente.findByUsernameAndPassword(username, password);
+        if (utenteLoggato == null) {
+            model.addAttribute("error", "username o password errati");
+            return "formLogin.html";
+        } else {
+            session.setAttribute("loggato", "ok");
+            session.setAttribute("utente", utenteLoggato);
+            Boolean admin = utenteLoggato.isAdmin();
 
-@PostMapping("/login")
-public String login(@RequestParam("username") String username,
-@RequestParam("password") String password, Model model,
-HttpSession session){
-    Utente utenteLoggato = serviceUtente.findByUsernameAndPassword(username , password);
-    if (utenteLoggato == null){
-        model.addAttribute("error","username o password errati");
-        return "formLogin.html";
-    } else {                   
-        session.setAttribute("loggato", "ok");
-        session.setAttribute("utente", utenteLoggato);
-        Boolean admin = utenteLoggato.isAdmin();
+            // return "redirect:/";
 
-        //return "redirect:/";
-
-        if(admin) {
-            session.setAttribute("admin", admin);
-            return "redirect:/home";
-        }else if(!admin){
-            return "redirect:/home";
+            if (admin) {
+                session.setAttribute("admin", admin);
+                return "redirect:/home";
+            } else if (!admin) {
+                return "redirect:/home";
+            }
         }
-        }return "redirect:/home";
+        return "redirect:/home";
     }
-    
+
     @GetMapping("/paymethod")
     public String carrello() {
         return "cartaDiCredito.html";
     }
 
-        @ModelAttribute("myList")
-        public List<String> createMyList() {
+    @GetMapping("/acquistoeffettuato")
+    public String acquisto(HttpSession session) {
+        session.setAttribute("carrello", new ArrayList<Entity>());
+        return "redirect:/acquisti";
+    }
+
+    @ModelAttribute("myList")
+    public List<String> createMyList() {
         return new ArrayList<>();
     }
 }
--- src/main/resources/static/js/carrello.js
+++ src/main/resources/static/js/carrello.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
     // Salva l'URL della pagina precedente in una variabile
     var previousUrl = document.referrer;
 
@@ -17,18 +17,48 @@ document.addEventListener('DOMContentLoaded', function() {
     if (storedPreviousUrl) {
         document.getElementById('previousUrl').textContent = 'URL della pagina precedente: ' + storedPreviousUrl;
     }
-});
+});*/
 
 function goBack() {
+    /*var storedPreviousUrl=localStorage.getItem('previousUrl');
+    console.log(storedPreviousUrl);*/
+    //window.history.go(-1);
+    /*while(storedPreviousUrl.includes("lista-carrello")){
+        storedPreviousUrl=storedPreviousUrl.substring(0,storedPreviousUrl.lastIndexOf('/'));
+        
+    }
     // Recupera l'URL della pagina precedente dal localStorage
-    var storedPreviousUrl = localStorage.getItem('previousUrl');
+    //var storedPreviousUrl = localStorage.getItem('previousUrl');
+    window.location.href = storedPreviousUrl;
 
     // Verifica se l'URL della pagina precedente non è vuoto
-    if (storedPreviousUrl) {
+    /*if (storedPreviousUrl) {
+        console.log(storedPreviousUrl);
+        var indexUrl=storedPreviousUrl.lastIndexOf('/');
+        var page=storedPreviousUrl.substring(indexUrl+1);
+        if(page=="lista-carrello"){
+    
+        }
         // Reindirizza l'utente alla pagina precedente
-        window.location.href = storedPreviousUrl;
+        //window.location.href = storedPreviousUrl;
     } else {
         console.log('Nessuna pagina precedente trovata.');
-    }
-  
+    }*/
+    /*var storedPreviousUrl=localStorage.getItem('carrelloPreviousUrl');
+    console.log(storedPreviousUrl);
+    window.location.assign(storedPreviousUrl);*/
+    console.log(localStorage.getItem('previousUrl'));
+    window.location.assign(localStorage.getItem('previousUrl'));
+}
+
+function removeHistoryPage(){
+    /*console.log(localStorage.getItem('previousUrl'));
+    try {
+        localStorage.setItem('carrelloPreviousUrl',localStorage.getItem('previousUrl'));
+    history.replaceState(null, null, localStorage.getItem('previousUrl'));
+    } catch (error) {
+        console.error('Error setting item in localStorage:', error);
+    }*/
+    history.replaceState(null, null, window.location.href);
+    
 }
